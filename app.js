ec5862e9464519a16572655a5a14ab85406f5553
Adnan Khan <adnan.khan2@kyndryl.com>
2023-07-30 17:20:04 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+// async function getPost() {
+//   const res = await axios.get("https://jsonplaceholder.typicode.com/posts/1");
+//   console.log(res.data);
+// }
+
+// getPost();
+
+console.log("hello");
+console.log("hello");
